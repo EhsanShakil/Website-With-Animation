5a20a821ae4d3c24502ca2456530942545088983
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-28 23:53:29 +0500

    Updating

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,32 +3,41 @@ import "../../App.css";
 
 const Header = () => {
   return (
-    <div className="navbar">
-      <ul className="nav">
-        <li>
-          <a href="#home">Home</a>
-        </li>
-      </ul>
-      <ul className="nav">
-        <li>
-          <a href="#services">Services</a>
-        </li>
-      </ul>
-      <ul className="nav">
-        <li>
-          <a href="#about">About</a>
-        </li>
-      </ul>
-      <ul className="nav">
-        <li>
-          <a href="#team">Team</a>
-        </li>
-      </ul>
-      <ul className="nav">
-        <li>
-          <a href="#contact">Contact Us</a>
-        </li>
-      </ul>
+    <div className="header">
+      <div>
+        <ul className="nav">
+          <li>
+            <a href="/">LOGO</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar">
+        <ul className="nav">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+        </ul>
+        <ul className="nav">
+          <li>
+            <a href="#services">Services</a>
+          </li>
+        </ul>
+        <ul className="nav">
+          <li>
+            <a href="#about">About</a>
+          </li>
+        </ul>
+        <ul className="nav">
+          <li>
+            <a href="#team">Team</a>
+          </li>
+        </ul>
+        <ul className="nav">
+          <li>
+            <a href="#contact">Contact Us</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/Components/Screens/Services.js
+++ src/Components/Screens/Services.js
@@ -1,9 +1,66 @@
 import React from "react";
 import "../../App.css";
+
+const webdevelopment = require("./pictures/web-development.png");
+const appdevelopment = require("./pictures/3.png");
+const softwaredevelopment = require("./pictures/4.png");
+
 function Services() {
   return (
     <div className="Screens" id="services">
-      Services
+      <div className="heading">
+        <h1>Our Services</h1>
+      </div>
+      <div className="services-cards">
+        <div className="service-card">
+          <img
+            className="services-image"
+            src={webdevelopment}
+            alt="web development"
+          />
+          <h2 className="services-subheading">Web Development</h2>
+          <p className="services-paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+        <div className="service-card">
+          <img
+            className="services-image"
+            src={appdevelopment}
+            alt="app development"
+          />
+          <h2 className="services-subheading">App Development</h2>
+          <p className="services-paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+        <div className="service-card">
+          <img
+            className="services-image"
+            src={softwaredevelopment}
+            alt="software development"
+          />
+          <h2 className="services-subheading">Software Development</h2>
+          <p className="services-paragraph">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
