b4b3158dafda43d0872384a1fce0f36d39dab351
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-07-29 15:36:02 +0500

    Updating

--- src/Components/Screens/Contact.js
+++ src/Components/Screens/Contact.js
@@ -3,7 +3,17 @@ import React from "react";
 function Contact() {
   return (
     <div className="Screens" id="contact">
-      Contact
+      <div className="heading">
+        <h1>Contact Us</h1>
+      </div>
+      <div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <form></form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Screens/Team.js
+++ src/Components/Screens/Team.js
@@ -1,13 +1,40 @@
 import React from "react";
 import "../../App.css";
+
+const teamleader = require("./pictures/teamleader.png");
+const webdeveloper = require("./pictures/webdeveloper.png");
+const appdeveloper = require("./pictures/appdeveloper.png");
+
 function Team() {
   return (
     <div className="Screens" id="team">
       <div className="heading">
         <h1>Our Team</h1>
       </div>
-      <div className="heading">
-        <h1>Our Team</h1>
+      <div className="team">
+        <div className="teamcard">
+          <img className="teamimage" src={teamleader} alt="team leader Image" />
+          <h3>Name</h3>
+          <h2>Team Leader</h2>
+        </div>
+        <div className="teamcard">
+          <img
+            className="teamimage"
+            src={webdeveloper}
+            alt="web developer Image"
+          />
+          <h3>Name</h3>
+          <h2>Web Developer</h2>
+        </div>
+        <div className="teamcard">
+          <img
+            className="teamimage"
+            src={appdeveloper}
+            alt="app developer Image"
+          />
+          <h3>Name</h3>
+          <h2>App Developer</h2>
+        </div>
       </div>
     </div>
   );
