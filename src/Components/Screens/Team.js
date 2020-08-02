7083a0565290d54e5df3849cc9d8a62ab5dd6f98
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-02 16:26:21 +0500

    updating

--- src/Components/Screens/Team.js
+++ src/Components/Screens/Team.js
@@ -11,20 +11,42 @@ function Team() {
   return (
     <div className="Screens" id="team">
       <div className="heading">
-        <h1 ref={r1}>Our Team</h1>
+        <h1
+          data-aos="fade-left"
+          data-aos-delay="500"
+          data-aos-once="false"
+          data-aos-easing="ease-in-out"
+        >
+          Our Team
+        </h1>
       </div>
       <div className="team">
-        <div className="teamcard">
+        <div
+          className="teamcard"
+          data-aos="flip-up"
+          data-aos-delay="600"
+          data-aos-once="false"
+        >
           <img className="teamimage" src={teamleader} alt="team leader" />
           <h3>Name</h3>
           <h2>Team Leader</h2>
         </div>
-        <div className="teamcard">
+        <div
+          className="teamcard"
+          data-aos="flip-up"
+          data-aos-delay="700"
+          data-aos-once="false"
+        >
           <img className="teamimage" src={webdeveloper} alt="web developer" />
           <h3>Name</h3>
           <h2>Web Developer</h2>
         </div>
-        <div className="teamcard">
+        <div
+          className="teamcard"
+          data-aos="flip-up"
+          data-aos-delay="800"
+          data-aos-once="false"
+        >
           <img className="teamimage" src={appdeveloper} alt="app developer" />
           <h3>Name</h3>
           <h2>App Developer</h2>
